fcf2ab70f1f6e15d0d885b796385d62aeb5e2ed6
BHW <kassadin34@gmail.com>
2024-04-15 14:31:24 +0900

    이제 실행 문제는 업는데 브레이크포인트가 안걸린다. 이 문제 수정해야 한다

--- run_front/src/App.vue
+++ run_front/src/App.vue
@@ -1,15 +1,21 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <button @click="handleClick">Click me</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
 
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  methods: {
+    handleClick() {
+      alert('Button clicked!');
+    }
   }
 }
 </script>
@@ -24,3 +30,14 @@ export default {
   margin-top: 60px;
 }
 </style>
+
+/*
+f1 누르고 open user setting 검색 후 아래 코드 추가
+그래야 1번 줄에 발생하는 애러 출력 안됨
+
+"eslint.workingDirectories": [
+    {
+      "mode": "auto"
+    }
+  ]
+*/
